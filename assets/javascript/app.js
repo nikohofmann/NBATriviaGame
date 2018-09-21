efe23aaf69c4ea080bef78a201899919062dd44a
nikohofmann <41592016+nikohofmann@users.noreply.github.com>
2018-09-20 19:07:35 -0600

    Initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,113 @@
+$("document").ready(function() {
+  var gameStarted = false;
+  var questions = [
+    {
+      question: "Which NBA player holds the record for most points scored in a single game?",
+      answers: {
+        a: "Michael Jordan",
+        b: "Wilt Chamberlain",
+        c: "Lebron James",
+        d: "Kobe Bryant",
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "John Stockton holds the career assists record in the NBA. How many assists did he have in his career?",
+      answers: {
+        a: "15,806",
+        b: "17,248",
+        c: "14,617",
+        d: "16,091",
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: "Which NBA team has the most championships?",
+      answers: {
+        a: "Los Angeles Lakers",
+        b: "Chicago Bulls",
+        c: "Boston Celtics",
+        d: "Golden State Warriors",
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Atlanta is the fourth home of the NBA's Hawks. Which of the following is NOT one of their previous locations?",
+      answers: {
+        a: "East Moline, Illinois",
+        b: "Milwaukee, Wisconsin",
+        c: "St. Louis, Missouri",
+        d: "Pittsburgh, Pennsylvania",
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "Which NBA player holds the record for most points scored in a single game?",
+      answers: {
+        a: "Michael Jordan",
+        b: "Wilt Chamberlain",
+        c: "Lebron James",
+        d: "Kobe Bryant",
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "Which NBA player holds the record for most points scored in a single game?",
+      answers: {
+        a: "Michael Jordan",
+        b: "Wilt Chamberlain",
+        c: "Lebron James",
+        d: "Kobe Bryant",
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "Which NBA player holds the record for most points scored in a single game?",
+      answers: {
+        a: "Michael Jordan",
+        b: "Wilt Chamberlain",
+        c: "Lebron James",
+        d: "Kobe Bryant",
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "Which NBA player holds the record for most points scored in a single game?",
+      answers: {
+        a: "Michael Jordan",
+        b: "Wilt Chamberlain",
+        c: "Lebron James",
+        d: "Kobe Bryant",
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "Which NBA player holds the record for most points scored in a single game?",
+      answers: {
+        a: "Michael Jordan",
+        b: "Wilt Chamberlain",
+        c: "Lebron James",
+        d: "Kobe Bryant",
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "Which NBA player holds the record for most points scored in a single game?",
+      answers: {
+        a: "Michael Jordan",
+        b: "Wilt Chamberlain",
+        c: "Lebron James",
+        d: "Kobe Bryant",
+      },
+      correctAnswer: "b"
+    },
+  ];
+
+
+
+
+
+
+
+
+});
